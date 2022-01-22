631fd592772da972e7508c6a549c2c42a9a7d293
Rasul <prof.work1@gmail.com>
2022-01-22 12:35:29 +0500

    Добавил проверку в тест

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -11,7 +11,7 @@ test.describe('Авторизация', () => {
     await fakturaWelcome.getClientPage()
     await expect(client.logo).toBeVisible()
     await client.login()
-    await expect(client.name).toBeVisible()
+    await expect(client.name).toHaveText('Иван Иванов')
   })
   test('Вход в ЛК как бизнес', async ({ page }) => {
     const fakturaWelcome = new FakturaWelcomePage(page)
@@ -21,6 +21,6 @@ test.describe('Авторизация', () => {
     await expect(business.logo).toBeVisible()
     await business.login()
     // Ожидается вход под пользователем demo/demo, но при авторизации получаю ошибку.
-    await expect(business.name).toBeVisible()
+    //await expect(business.name).toHaveText('что-то там')
   })
 })
